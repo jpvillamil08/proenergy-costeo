d044a51920174962bb4097729bb6c28341d756bd
jpvillamil08 <161524504+jpvillamil08@users.noreply.github.com>
2026-08-27 08:36:17 -0500

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-// Evita que una vista asincrona (fetch en curso) siga mutando el DOM despues
-// de que el usuario ya navego a otra pantalla.
-export function stillMounted(el) {
-  return !!el && document.body.contains(el);
-}
